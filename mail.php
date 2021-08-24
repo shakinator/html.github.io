5341f27811c83c3e6a4bf692270bccc9ff618e55
shakinator <65782416+shakinator@users.noreply.github.com>
2021-08-24 10:41:09 +0530

    commit 5

--- mail.php
+++ mail.php
@@ -1,10 +1,11 @@
-<?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $recipient = "saksham12g@gmail.com";
-    $subject = "Contact Form";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $mailheader) or die("Error!");
-    echo "Thank You!";
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$jobslist = $POST['jobslist'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "saksham12g@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
